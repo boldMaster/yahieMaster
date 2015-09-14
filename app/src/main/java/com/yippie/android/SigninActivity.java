14b2bf73831831459b637cc0922126fe8ecfbbc0
boldMaster <mvpchen7@gmail.com>
2015-09-14 21:17:36 +0800

    Paper Sign In Send http request

--- app/src/main/java/com/yippie/android/SigninActivity.java
+++ app/src/main/java/com/yippie/android/SigninActivity.java
@@ -1,33 +1,51 @@
 package com.yippie.android;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yippie.android.classes.User;
+import com.yippie.android.library.HttpRequestController;
+import com.yippie.android.library.HttpRequestInterface;
+import com.yippie.android.library.HttpReturnObject;
+import com.yippie.android.library.Utility;
 import com.yippie.android.classes.FacebookAuth;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SigninActivity extends Activity
+public class SigninActivity extends Activity implements HttpRequestInterface
 {
-    EditText emailUsername;
-    EditText password;
-    RelativeLayout btnSignin;
-    RelativeLayout btnFBSignin;
-    RelativeLayout btnGoogleSignin;
-    TextView btnForgetPass;
+    protected EditText emailUsername;
+    protected EditText password;
+    protected RelativeLayout btnSignin;
+    protected RelativeLayout btnFBSignin;
+    protected RelativeLayout btnGoogleSignin;
+    protected TextView btnForgetPass;
+    protected ProgressDialog pDialog;
     private FacebookAuth objFacebookAuth;
+    private static final String SIGN_IN_CALLER = "sign_in_caller";
 
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -76,9 +94,9 @@ public class SigninActivity extends Activity
         btnForgetPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(getApplicationContext(), "Forget password Clicked!", Toast.LENGTH_SHORT).show();
-
+                // Open the new Intent to forgot password page
+                Intent intent = new Intent(getApplicationContext(), ForgetPasswordActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -86,7 +104,12 @@ public class SigninActivity extends Activity
     /**
      * Private function that perform the account registration
      */
-    private void loginUser() {
+    private void loginUser()
+    {
+        // Dismiss soft keyboard
+        InputMethodManager input = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+        input.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+
         // Extract the value from edittext
         String emailusername = emailUsername.getText().toString();
         String pass = password.getText().toString();
@@ -99,14 +122,9 @@ public class SigninActivity extends Activity
 
         if(isValid)
         {
-            // Login user
-            User.loginUser(emailusername,emailusername,pass);
-
-            // Launch Sign In Activity
-            // Note: Remember to finish(). User shall not be able to return to this page after they login
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
+            // Launch SignIn user Async Task
+            SignInUser task = new SignInUser(emailusername,pass);
+            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         }
         else
         {
@@ -166,20 +184,140 @@ public class SigninActivity extends Activity
             validateObj.setStatus(false);
         }
 
-        if(validateObj.getStatus())
-        {
-            // Enter here if there are no problem found in email and password
-            // We will try to get user information from database
-            User user = User.getUser(emailUsername,emailUsername,pass);
-
-            if(user==null)
-            {
-                validateObj.setErrMsg("Invalid email, username or password combination.");
-                validateObj.setStatus(false);
-            }
-        }
-
         return validateObj;
+    }
+
+    @Override
+    public void HttpRequestPreExecuteDelegate()
+    {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run()
+            {
+                // Showing progress dialog before perform the HTTP REQUEST
+                pDialog = new ProgressDialog(SigninActivity.this);
+                pDialog.setMessage("Signing You in.. Please wait..");
+                pDialog.setIndeterminate(true);
+                pDialog.setCancelable(false);
+                pDialog.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void HttpRequestDoInBackgroundDelegate(HttpReturnObject response) {}
+
+    @Override
+    public void HttpRequestDoInBackgroundErrorDelegate(HttpReturnObject response) {}
+
+    @Override
+    public void HttpRequestProgressUpdateDelegate(HttpReturnObject response) {}
+
+    @Override
+    public void HttpRequestOnPostExecuteUpdateDelegate(HttpReturnObject response)
+    {
+        try
+        {
+            // Extract the result from the
+            JSONObject responseJson = response.getJSON();
+            // Get Status code from returned JSON
+            Integer statusCode = responseJson.getInt("status_code");
+
+            switch(statusCode)
+            {
+                case 200:
+                {
+                    // Enter here if status code is 200 (Success)
+                    // Extract the return_vars
+                    JSONObject returnVars = responseJson.getJSONObject("return_vars");
+                    // Extract resutl from the return vars
+                    Boolean is_valid = returnVars.getBoolean("status");
+
+                    if(is_valid)
+                    {
+                        // Enter here if the request is valid and status is true
+                        // Get caller name
+                        String caller = responseJson.getString("caller");
+
+                        switch(caller)
+                        {
+                            // Sign in request
+                            case SIGN_IN_CALLER:
+                            {
+                                Boolean status = responseJson.getBoolean("status");
+
+                                if(status)
+                                {
+                                    // Enter here if the user credential are valid
+                                    String firstName = responseJson.getString("first_name");
+                                    String lastName = responseJson.getString("last_name");
+
+                                    // Sandbox testing usage
+                                    Toast.makeText(this, "Welcome back! " + firstName + " " + lastName, Toast.LENGTH_LONG).show();
+                                }
+                                else
+                                {
+                                    // Display error in this section
+                                    JSONArray errorList = returnVars.getJSONArray("errors");
+                                    List<String> errMsgList = new ArrayList<>();
+
+                                    for(Integer i = 0; i < errorList.length(); i++) {
+                                        // Get the err msg and store into array list
+                                        errMsgList.add(errorList.getString(i));
+                                    }
+
+                                    // Join the error message with \n
+                                    String errMsg = TextUtils.join("\n", errMsgList);
+
+                                    // Show error message by usign Toast
+                                    Toast.makeText(this, errMsg,Toast.LENGTH_LONG).show();
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Display error in this section
+                        JSONArray errorList = returnVars.getJSONArray("errors");
+                        List<String> errMsgList = new ArrayList<>();
+
+                        for(Integer i = 0; i < errorList.length(); i++) {
+                            // Get the err msg and store into array list
+                            errMsgList.add(errorList.getString(i));
+                        }
+
+                        // Join the error message with \n
+                        String errMsg = TextUtils.join("\n", errMsgList);
+
+                        // Show error message by usign Toast
+                        Toast.makeText(this, errMsg,Toast.LENGTH_LONG).show();
+                    }
+
+
+                    break;
+                }
+
+                case 403:
+                default:
+                    // Enter here if the status code is 403 (unauthorized)
+                    Toast.makeText(this, "You do not have permission to perform this action.", Toast.LENGTH_LONG).show();
+                    break;
+            }//switch(statusCode)
+
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }//try
+
+        // Dismiss the Progress Dialog if not null
+        if(pDialog != null)
+        {
+            pDialog.dismiss();
+        }
     }
 
     /**
@@ -222,6 +360,48 @@ public class SigninActivity extends Activity
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
+        }
+    }
+    /**
+     * This is an AsyncTask to handle the following process:
+     * 1. Validate user crdential at server
+     */
+    public class SignInUser extends AsyncTask<Void, Void, Void>
+    {
+        //String baseUrl = "localhost";
+        String baseUrl = "192.168.1.11";
+        String email;
+        String pass;
+
+        /**
+         * Constructor
+         * @param email - The email of the user
+         * @param pass  - The password of the account (MD5)
+         */
+        public SignInUser(String email, String pass)
+        {
+            this.email = email;
+            this.pass = Utility.MD5(pass);
+        }
+
+        @Override
+        protected Void doInBackground(Void...voids)
+        {
+            // Construct parameter to be sent over HTTP Request
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new BasicNameValuePair("api_key", Utility.getPublicAccessKey()));	// API Key
+            params.add(new BasicNameValuePair("caller", SIGN_IN_CALLER));	                // Caller
+            params.add(new BasicNameValuePair("email", email));	                            // User ID
+            params.add(new BasicNameValuePair("pass", pass));                               // User ID of the conversation user
+
+            // Trigger AsyncTask
+            HttpRequestController httpClient = new HttpRequestController(SigninActivity.this);
+            String httpURL = "http://" + baseUrl +"/laravel/public/android/v1/user/signin";
+
+            // Execute Get Conversation User List AsyncTask
+            httpClient.performAsynRequest(httpURL, params);
+
+            return null;
         }
     }
 }
